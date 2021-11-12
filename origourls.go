ab97e528a360ec13a82ee03b984ef64204fe7230
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2021-11-12 11:56:07 +0100

    Initial commit - some comments to describe idea

---
+++ origourls.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Concept - must be able to store 'key' (urls.os.lol/kjoremiljo) -> https://meet.google.com/iur-phvy-hit or something
+// store this data in a key value store ( we will use Amazon S3)
+
+func main() {
+	fmt.Println("Welcome to origourls!")
+}
