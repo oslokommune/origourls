11b16604ab0648fed5d199475ac73167f9486482
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2021-11-15 09:48:48 +0100

    Some more comments and ignore .env

--- origourls.go
+++ origourls.go
@@ -5,6 +5,14 @@ import "fmt"
 // Concept - must be able to store 'key' (urls.os.lol/kjoremiljo) -> https://meet.google.com/iur-phvy-hit or something
 // store this data in a key value store ( we will use Amazon S3)
 
+// Origo prod , use AWS lambda,  // where to deploy lambda code?
+// iac-repo okctl-prod legge inn terraform / cloud formation der.
+
+// push to aws lambad somehow  | check how it's done with postgres key rotator.  ---
+
+// Use terraform or cloudformation, place directory at  ---
+
+
 func main() {
 	fmt.Println("Welcome to origourls!")
 }
